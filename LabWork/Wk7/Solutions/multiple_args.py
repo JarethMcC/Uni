2592bc9bd1be378fbfec1fe28000ac6cd81d9e2b
Jareth <mccardell-j@ulster.ac.uk>
2020-11-13 11:09:47 +0000

    Initial LabWork commit

---
+++ LabWork/Wk7/Solutions/multiple_args.py
@@ -0,0 +1,17 @@
+
+# This program demonstrates a function that accepts
+# two arguments.
+
+def main():
+    print('21 divided by 23 is')
+    show_division(21, 23)
+
+# The show_division function accepts two arguments
+# and displays their ratio .
+def show_division(num1, num2):
+    result = num1 / num2
+    print(result)
+
+# Call the main function.
+main()
+

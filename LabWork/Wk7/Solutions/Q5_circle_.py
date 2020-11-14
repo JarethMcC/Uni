decd16bc5bce6e4121c478e35393fafc8640c132
Jareth <mccardell-j@ulster.ac.uk>
2020-11-14 01:55:20 +0000

    Q5_circle.py, fixed bugs
    - Removed unnecessary return statements
    - Changed user input to expect y instead of 1
    - Added a space to start of all comments
    - Formatted area and circumference output to 2 decimal places

--- LabWork/Wk7/Solutions/Q5_circle_.py
+++ LabWork/Wk7/Solutions/Q5_circle_.py
@@ -1,41 +1,39 @@
-#Import functions from the circle.py file in the same folder
+# Import functions from the circle.py file in the same folder
 from circle import area
 from circle import circumference
 
-#The stillRunning function allows the user to continue to check for new numbers
-#without having to restart the program
+# The stillRunning function allows the user to continue to check for new numbers
+# without having to restart the program
 def stillRunning():
-    yOrN = input('if you would like to calculate another number type 1, if not press enter: ')
-    if yOrN == 1:
+    yOrN = input('if you would like to calculate another number type y, if not press enter: ')
+    if yOrN == 'y':
         print('1. Area')
         print('2. Circumference')
-        return True
     else:
         print('Thank you for using the program!')
         exit()
-    return status
 
-#Initial text the user will see
+# Initial text the user will see
 print('Hello this program will allow you calculate the area or circumference of a circle')
 print('Please select which you would like to calculate')
 print('1. Area')
 print('2. Circumference')
 print('3. Exit')
 
-#Setting the variable for the while loop to be True so that it runs
+# Setting the variable for the while loop to be True so that it runs
 running = True
 
-#This while loop will first ask which option the user would like to choose and then executes it
-#The stillRunning function is called at the end of each option to see if the user would like to continue
+# This while loop will first ask which option the user would like to choose and then executes it
+# The stillRunning function is called at the end of each option to see if the user would like to continue
 while running == True:
     option = int(input(''))
     if option == 1:
         radiusA = int(input('Please enter the radius of the circle you would like to calculate the area of: '))
-        print(area(radiusA))
+        print('%.2f' %  area(radiusA))
         stillRunning()
     elif option == 2:
         radiusC = int(input('Please enter the circumference of the circle to calculate the area of:'))
-        print(circumference(radiusC))
+        print('%.2f' %  circumference(radiusC))
         stillRunning()
     elif option == 3:
         print('Thank you for using the program!')
